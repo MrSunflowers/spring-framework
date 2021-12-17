5f8b3f39ebce1ab973fccad24e5ad4d732b6304e
MrSunflowers <56327989+MrSunflowers@users.noreply.github.com>
2021-12-17 17:27:31 +0800

    2021年12月17日17:27:24

---
+++ spring-test/src/test/java/org/springframework/myTest/TestLifecycle/TestLifecycle.java
@@ -0,0 +1,34 @@
+package org.springframework.myTest.TestLifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.context.Lifecycle;
+
+/**
+ * @Description
+ * @Author lyk
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2021/12/17
+ */
+public class TestLifecycle implements Lifecycle {
+
+	private boolean running = false;
+
+	@Override
+	public void start() {
+		this.running = true;
+		System.out.println("TestLifecycle is start");
+	}
+
+	@Override
+	public void stop() {
+		this.running = false;
+		System.out.println("TestLifecycle is stop");
+	}
+
+	@Override
+	public boolean isRunning() {
+		return this.running;
+	}
+
+}
