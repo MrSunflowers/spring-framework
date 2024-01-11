ad2aa0822a1727c8fc39440cee33bcf5445bad40
MrSunflowers <56327989+MrSunflowers@users.noreply.github.com>
2024-01-11 17:56:40 +0800

    mysql

--- spring-test/src/test/java/org/springframework/myTest/jdbc/test1/jdbcMain.java
+++ spring-test/src/test/java/org/springframework/myTest/jdbc/test1/jdbcMain.java
@@ -2,12 +2,18 @@ package org.springframework.myTest.jdbc.test1;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.Objects;
+
 public class jdbcMain {
 
 	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext context =
 				new ClassPathXmlApplicationContext("myTestResources/jdbc/test1/applicationContext_jdbc.xml");
-
-
+		DataSource dataSource = context.getBean("dataSource", DataSource.class);
+		Connection connection = dataSource.getConnection();
+		Objects.requireNonNull(connection);
+		connection.close();
 	}
 }
